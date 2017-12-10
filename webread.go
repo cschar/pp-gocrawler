16bee0d9cd5efae891ad4de5c349aad6cfae7e0f
cschar <codyscharfe@gmail.com>
2017-12-10 17:11:33 -0500

    webworkers fetching images

--- webread.go
+++ webread.go
@@ -13,6 +13,13 @@ import (
     //"go/doc"
     //"time"
     //"go/doc"
+    "os"
+    "bufio"
+    "net/http"
+    "io/ioutil"
+    "strings"
+    "sync"
+    "sync/atomic"
 )
 
 type WebResult struct {
@@ -66,7 +73,7 @@ func ScrapeImageLinks(url string) *WebResult {
 }
 
 //https://gobyexample.com/worker-pools
-func QueueWorkers() {
+func ScraperWorkers() {
 
     webjobs := make(chan string, 100)
     webresults := make(chan WebResult, 100) //TODO make *WebResult
@@ -84,64 +91,123 @@ func QueueWorkers() {
     close(webjobs) //close so range wont block (awaiting next sender)
 
 
+    f, err := os.Create("scrape.txt")
+    check(err)
+    defer f.Close()
+    w := bufio.NewWriter(f)
+
     //await web workers to finish first N jobs
     for a:=1; a <= 12; a++{
         result := <-webresults
-        fmt.Println("worker result: ")
+        fmt.Println("worker result: ", result.num)
         for i:=0; i<result.num;i++{
-            fmt.Println(result.links[i])
+            //fmt.Println(result.links[i])
+            w.WriteString(result.links[i])
+            w.WriteString("\n")
         }
         //fmt.Println("Got webresult :\n", result)
     }
+    w.Flush()
 
 }
 
 
-
-
-
-func ExampleScrape() {
-    doc, err := goquery.NewDocument("http://kamadenu.blogspot.ca/2016/02/")
-    //doc, err := goquery.NewDocument("http://kamadenu.blogspot.ca")
-  //doc, err := goquery.NewDocument("http://metalsucks.net")
-  if err != nil {
-    log.Fatal(err)
-  }
-
-
-    MAX_IMG_PER_PAGE := 40
-    links := make([]string, MAX_IMG_PER_PAGE)
-  // Find the review items
-    idx := 0
-    doc.Find(".separator  a").Each(func(i int, s *goquery.Selection) {
-    // For each item found, get the band and title
-    //band := s.Find("a").Text()
-    //title := s.Find("i").Text()
-    //fmt.Printf("Review %d: %s - %s\n", i, band, title)
-      fmt.Println(s.Html())
-
-      fmt.Println()
-      s.Find("img").Each(func(i int, s1 *goquery.Selection) {
-          link, ok := s1.Attr("src")
-          fmt.Println(link, ok)
-          if ok{
-              links[idx] = link
-              idx += 1
-          }
-      })
-
-  })
-
-    fmt.Println("collected links:")
-    for i:=0; i<idx; i++{
-        val := links[i]
-        fmt.Println(i, val)
+func check(e error) {
+    if e != nil {
+        panic(e)
     }
 }
 
 
-func main() {
-    QueueWorkers()
-    //ExampleScrape()
+var wg sync.WaitGroup
+var saves uint64 = 0
+var errors uint64 = 0
 
+func ImageFetchWorker(id string, jobs <-chan string){
+    defer wg.Done()
+    fmt.Printf("worker %s started\n", id)
+
+
+    for image_url := range jobs {
+
+        n2 := strings.TrimSpace(image_url)
+        resp, err := http.Get(n2)
+        if resp.StatusCode == 404 || err != nil{
+            //panic(404)
+            atomic.AddUint64(&errors, 1)
+            continue
+        }
+        //
+        bytes, err := ioutil.ReadAll(resp.Body)
+        if err != nil{
+            atomic.AddUint64(&errors, 1)
+            continue
+            //panic(err)
+        }
+
+
+        stringSlice := strings.Split(image_url, "/")
+        name := stringSlice[len(stringSlice) - 1]
+        //name = name[:len(name)-4] //.jpg removal
+
+        img, _ := os.Create(fmt.Sprintf("images/img%s", name))
+        img.Write(bytes)
+        img.Close()
+
+
+        //https://gobyexample.com/mutexes
+        //https://gobyexample.com/atomic-counters
+        atomic.AddUint64(&saves, 1)
+        current := atomic.LoadInt64(&saves)
+        if current % 20 == 0{
+            fmt.Println("worker %s @ %d saves", id, current)
+        }
+
+    }
+}
+
+
+//https://nathanleclaire.com/blog/2014/02/15/how-to-wait-for-all-goroutines-to-finish-executing-before-continuing/
+func FetcherWorkers(){
+    f, err := os.Open("scrape.txt")
+    check(err)
+    defer f.Close()
+
+
+    //channels can become full, give 100 size for now
+    jobs := make(chan(string), 100)
+
+    //queue workers
+    for w:=1; w<=3; w++{
+        wg.Add(1)
+        go ImageFetchWorker(fmt.Sprintf("worker%d",w), jobs)
+    }
+    //close(jobs) //close so range wont block (awaiting next sender)
+
+    //queue jobs
+    scanner := bufio.NewScanner(f)
+    i := 0
+    for scanner.Scan() {
+
+        if i % 100 == 0{fmt.Println("queued %d jobs", i)}
+
+        jobs<- scanner.Text()
+        i++
+        if i > 100{
+            break
+        }
+    }
+
+    fmt.Println("waiting")
+    close(jobs)
+    wg.Wait()
+
+
+    fmt.Printf("done with %d image saves\n", saves)
+}
+
+func main() {
+    //ScraperWorkers()
+
+    FetcherWorkers()
 }
